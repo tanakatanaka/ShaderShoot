e36216b6ecbaee8a6f82202f68d9b4b0faf4db9a
tanakatanaka <aiuro2003haru@gmail.com>
2022-09-19 15:15:07 +0900

    first

---
+++ Source/ShaderShoot/ShaderShootGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ShaderShootGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AShaderShootGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AShaderShootGameMode();
+};
+
+
+

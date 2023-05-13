3705aaf172c7430b2a39020662268e54546a921f
tanakatanaka <aiuro2003haru@gmail.com>
2023-05-14 01:33:41 +0900

    cppをbp化

---
+++ Source/ShaderShoot/Private/Target.cpp
@@ -0,0 +1,33 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Target.h"
+
+// Sets default values
+ATarget::ATarget()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
+
+}
+
+// Called when the game starts or when spawned
+void ATarget::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ATarget::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void  ATarget::NotifyHit(UPrimitiveComponent* MyComp, AActor* Other, UPrimitiveComponent* OtherComp, bool bSelfMoved, FVector HitLocation, FVector HitNormal, FVector NormalImpulse, const FHitResult& Hit)
+{
+	Super::NotifyHit( MyComp, Other, OtherComp, bSelfMoved, HitLocation, HitNormal, NormalImpulse, Hit);
+	
+	K2_DestroyActor();
+}

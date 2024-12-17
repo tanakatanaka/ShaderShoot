d14f617e55e9896ae05a32a6f52e8164338664d8
tanatana <aiueo2003haru@gmail.com>
2024-12-18 00:11:42 +0900

    中心にポインターを追加

---
+++ Source/ShaderShoot/Public/ShaderShootHUD.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/HUD.h"
+#include "ShaderShootHUD.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SHADERSHOOT_API AShaderShootHUD : public AHUD
+{
+	GENERATED_BODY()
+	
+};
